5b66d73dac6990f7fd299d24b78ab85759e57101
Oskar Krawczyk <oskar@krawczyk.io>
2016-10-07 10:39:10 +0200

    Code cleanup

--- browser.js
+++ browser.js
@@ -3,7 +3,6 @@ const ipc      = electron.ipcRenderer
 const storage  = electron.remote.require("./storage")
 
 // const NativeNotification = Notification
-
 // Notification = (title, options) => {
 //   const notification = new NativeNotification(title, options)
 //   notification.addEventListener('click', () => {
@@ -12,49 +11,113 @@ const storage  = electron.remote.require("./storage")
 //
 //   return notification
 // }
+//
+// Notification.prototype = NativeNotification.prototype
+// Notification.permission = NativeNotification.permission
+// Notification.requestPermission = NativeNotification.requestPermission.bind(Notification)
 
-createElements = (htmlStr) => {
-  var frag = document.createDocumentFragment(),
-    temp = document.createElement("div");
-  temp.innerHTML = htmlStr
+class Overcast {
+  constructor(options){
+    this.elements = {}
+    this.templates = {}
 
-  while (temp.firstChild){
-    frag.appendChild(temp.firstChild)
-  }
-  return frag
-}
+    this.elements.player      = document.querySelector("audio")
+    this.elements.coverart    = document.querySelector(".fullart_container .art")
+    this.elements.episodecell = document.querySelectorAll(".episodecell")
+    this.elements.styles      = document.querySelectorAll("link[rel='stylesheet']")
+    this.elements.count       = this.elements.episodecell.length > 0 ? this.elements.episodecell.length : storage.get("active-episodes")
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  var player      = document.querySelector("audio");
-  var coverart    = document.querySelector(".fullart_container .art")
-  var episodecell = document.querySelectorAll(".episodecell")
-  var count       = episodecell.length > 0 ? episodecell.length : storage.get("active-episodes")
-
-  var icons = {
-    back:    "<svg viewBox='0 0 16 16'><path class='path1' d='M9 2.5v5l5-5v11l-5-5v5l-5.5-5.5z'></path></svg>",
-    play:    "<svg viewBox='0 0 16 16'><path class='path1' d='M3 2l10 6-10 6z'></path></svg>",
-    forward: "<svg viewBox='0 0 16 16'><path class='path1' d='M8 13.5v-5l-5 5v-11l5 5v-5l5.5 5.5z'></path></svg>"
-  }
-
-  var template = {
-    tabs: "<ul class='mainListTabs'><li><a class='active' href='#episodecell'>All active episodes</a></li><li><a href='#feedcell'>Your podcasts</a></li></ul>"
-  }
-
-  // we're on listing
-  if (document.location.href.indexOf("/podcasts") >= 0){
-    document.body.appendChild(createElements(template.tabs))
-    document.body.classList.add("page_podcasts")
-
-    var tabs = document.querySelectorAll(".mainListTabs a")
-
-    var showElements = (selector) => {
-      var conts = document.querySelectorAll("." + selector)
-      for (var iii = 0; iii < conts.length; ++iii){
-        conts[iii].classList.add("visible")
-      }
+    this.templates = {
+      tabs:    "<ul class='mainListTabs'><li><a class='active' href='#episodecell'>All active episodes</a></li><li><a href='#feedcell'>Your podcasts</a></li></ul>",
+      back:    "<svg viewBox='0 0 16 16'><path class='path1' d='M9 2.5v5l5-5v11l-5-5v5l-5.5-5.5z'></path></svg>",
+      play:    "<svg viewBox='0 0 16 16'><path class='path1' d='M3 2l10 6-10 6z'></path></svg>",
+      forward: "<svg viewBox='0 0 16 16'><path class='path1' d='M8 13.5v-5l-5 5v-11l5 5v-5l5.5 5.5z'></path></svg>"
     }
 
-    for (var i = 0; i < tabs.length; ++i){
+    // this.removeStylesheets(this.elements.styles)
+
+    // we're on listing
+    if (document.location.href.indexOf("/podcasts") >= 0){
+      this.podcastsList()
+    }
+
+    // update dock badge on each screen
+    ipc.send("update-badge", {
+      count: this.elements.count
+    })
+
+    this.player()
+  }
+
+  createElements(htmlStr){
+    let frag = document.createDocumentFragment(),
+      temp = document.createElement("div")
+    temp.innerHTML = htmlStr
+
+    while (temp.firstChild){
+      frag.appendChild(temp.firstChild)
+    }
+    return frag
+  }
+
+  removeStylesheets(styles){
+    let i
+    for (i = 0; i < styles.length; ++i){
+      let element = styles[i]
+      element.parentNode.removeChild(element)
+    }
+  }
+
+  player(){
+    if (this.elements.coverart){
+      this.addCoverart()
+    }
+
+    if (this.elements.player){
+      this.replacePlayerButtons()
+    }
+  }
+
+  replacePlayerButtons(){
+    let seekbackbutton    = document.querySelector("#seekbackbutton")
+    let seekforwardbutton = document.querySelector("#seekforwardbutton")
+
+    seekbackbutton.innerHTML    = this.templates.back
+    seekforwardbutton.innerHTML = this.templates.forward
+
+    this.elements.player.addEventListener("play", () => {
+      this.elements.coverart.classList.add("playing")
+      this.elements.shadowart.classList.add("playing")
+    })
+
+    this.elements.player.addEventListener("pause", () => {
+      this.elements.coverart.classList.remove("playing")
+      this.elements.shadowart.classList.remove("playing")
+    })
+  }
+
+  addCoverart(){
+
+    // inject shadow art
+    this.elements.shadowart           = document.createElement("img")
+    this.elements.shadowart.src       = this.elements.coverart.src
+    this.elements.shadowart.className = "shadowart"
+    this.elements.coverart.parentNode.appendChild(this.elements.shadowart)
+
+    // inject background art
+    let backgroundart                   = document.createElement("div")
+    backgroundart.style.backgroundImage = `url(${this.elements.coverart.src})`
+    backgroundart.className             = "backgroundart"
+    this.elements.coverart.parentNode.appendChild(backgroundart)
+  }
+
+  podcastsList(){
+    document.body.appendChild(this.createElements(this.templates.tabs))
+    document.body.classList.add("page_podcasts")
+
+    let tabs = document.querySelectorAll(".mainListTabs a")
+    let i
+    for (i = 0; i < tabs.length; ++i){
       tabs[i].addEventListener("click", (event) => {
         event.preventDefault()
         event.stopPropagation()
@@ -64,63 +127,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
         event.toElement.classList.add("active")
 
         // hide all episodes and podcasts
-        var allcells = document.querySelectorAll(".episodecell, .feedcell")
-        for (var ii = 0; ii < allcells.length; ++ii){
+        let allcells = document.querySelectorAll(".episodecell, .feedcell")
+        let ii
+        for (ii = 0; ii < allcells.length; ++ii){
           allcells[ii].classList.remove("visible")
         }
 
         // only show episodes or podcast, depending on the tab active
-        showElements(event.toElement.href.split("#")[1])
+        this.showElements(event.toElement.href.split("#")[1])
       })
     }
 
-    showElements("episodecell")
+    this.showElements("episodecell")
 
     // store the active episodes count for other screens that have
     // no access to that info
-    storage.set("active-episodes", count);
+    storage.set("active-episodes", this.elements.count);
   }
 
-  // update dock badge on each screen
-  ipc.send("update-badge", {
-    count: count
-  })
+  showElements(selector){
+    let elements = document.querySelectorAll(`.${selector}`)
+    let i
 
-  if (coverart){
-    
-    // inject shadow art
-    var shadowart       = document.createElement("img")
-    shadowart.src       = coverart.src
-    shadowart.className = "shadowart"
-    coverart.parentNode.appendChild(shadowart)
-
-    // inject background art
-    var backgroundart                   = document.createElement("div")
-    backgroundart.style.backgroundImage = "url(" + coverart.src + ")"
-    backgroundart.className             = "backgroundart"
-    coverart.parentNode.appendChild(backgroundart)
+    for (i = 0; i < elements.length; ++i){
+      elements[i].classList.add("visible")
+    }
   }
+}
 
-  if (player){
-    var seekbackbutton    = document.querySelector("#seekbackbutton")
-    var seekforwardbutton = document.querySelector("#seekforwardbutton")
-
-    seekbackbutton.innerHTML    = icons.back
-    seekforwardbutton.innerHTML = icons.forward
-
-    player.addEventListener("play", () => {
-      coverart.classList.add("playing")
-      shadowart.classList.add("playing")
-    })
-
-    player.addEventListener("pause", () => {
-      coverart.classList.remove("playing")
-      shadowart.classList.remove("playing")
-    })
-  }
-
-})
-
-// Notification.prototype = NativeNotification.prototype
-// Notification.permission = NativeNotification.permission
-// Notification.requestPermission = NativeNotification.requestPermission.bind(Notification)
+document.addEventListener("DOMContentLoaded", (event) => new Overcast())
